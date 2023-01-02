0f2f710bdb1efe98098026180280dd42148f72da
Jens Brörken <jens.broerken@icloud.com>
2023-01-02 05:05:03 +0100

    Update copyright

--- src/Revision.h
+++ src/Revision.h
@@ -1,5 +1,5 @@
 /**
- *  Copyright (C) 2022 The de.mrh.version Authors.
+ *  Copyright (C) 2022 - 2023 The de.mrh.version Authors.
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
